b8a825d634fb9a5010709ee56cf11981ff35e01d
lidefi87 <lidefi87@hotmail.com>
2025-04-09 10:56:24 +1000

    removing variable not used

--- new_workflow/run_dbpm_gridded.py
+++ new_workflow/run_dbpm_gridded.py
@@ -3,49 +3,56 @@
 #Loading libraries
 import os
 from glob import glob
-import xarray as xr
 import pandas as pd
+import xarray as xr
 import useful_functions as uf
 import dask
 from distributed import Client
 from multiprocessing import Process, freeze_support
+import json
+import numpy as np
+
 
 if __name__ == '__main__':
     freeze_support()
 
     #Start a cluster
-    client = Client()
+    client = Client(threads_per_worker = 1)
 
     #Do not print warning about large chunks
-    #dask.config.set({'array.slicing.split_large_chunks': False})
+    # dask.config.set({'array.slicing.split_large_chunks': False})
     #Reduce size of chunks to 100 MB
     #dask.config.set({'array.chunk-size': '100 MB'})
 
     ## Name of region and model resolution ----
     region = 'fao-48'
-    model_res = '1deg'
+    model_res = '025deg'
     
     ## Defining input and output folders ----
     base_folder = '/g/data/vf71/la6889/dbpm_inputs/weddell/'
     gridded_folder = os.path.join(base_folder, 'gridded_params', model_res)
-    out_folder = os.path.join(base_folder, 'run_fishing', model_res)
+    out_folder = os.path.join(base_folder, 'test', model_res)
+    #If output folder does not exist, it will create it
     os.makedirs(out_folder, exist_ok = True) 
 
     ## If starting DBPM run from a specific time step ----
     # Character: Year and month from when DBPM initialisation values should be loaded
     # If starting model for the first time, it should be set to None
-    init_time = '1960-11'
+    init_time = None
     
     ## Loading fixed DBPM parameters ----
-    ds_fixed = uf.loading_dbpm_inputs(gridded_folder)
+    ds_fixed = uf.loading_dbpm_fixed_inputs(gridded_folder)
     #Adding additional fixed DBPM parameters to dataset
     #Depth
-    depth = xr.open_zarr(glob(os.path.join(base_folder, 'gridded', 
+    depth = xr.open_zarr(glob(os.path.join(base_folder, 'gridded',
                                            model_res, '*obsclim_deptho_*'))[0])['deptho']
     ds_fixed['depth'] = depth
+    #Create a land mask from depth data
+    ds_fixed['mask'] = np.isfinite(depth)
     #Size bins in log10
     log10_size_bins_mat = xr.open_zarr('outputs/log10_size_bins_matrix.zarr/')['size_bins']
-    ds_fixed['log10_size_bins_mat'] = log10_size_bins_mat
+    ds_fixed['log10_size_bins'] = log10_size_bins_mat
+    ds_fixed['size_bin_vals'] = 10**log10_size_bins_mat
     #Removing datarrays added to fixed inputs
     del depth, log10_size_bins_mat
 
@@ -57,139 +64,86 @@ if __name__ == '__main__':
     
     ## Loading predator, detritivores and detritus initialisation data ----
     if init_time is None:
-        predators = xr.open_zarr(glob(os.path.join(gridded_folder, 
-                                                  'predators_*'))[0])['predators'] 
-        detritivores = xr.open_zarr(glob(os.path.join(gridded_folder, 
-                                                    'detritivores_*'))[0])['detritivores']
-        detritus = xr.open_zarr(glob(os.path.join(gridded_folder, 
-                                                  'detritus_*'))[0])['detritus']
+        ds_init = uf.loading_dbpm_biomass_inputs(gridded_folder)
     else:
-        predators = xr.open_dataarray(
-            glob(os.path.join(out_folder, f'predators_*_{init_time}.nc'))[0])
-        detritivores = xr.open_dataarray(
-            glob(os.path.join(out_folder, f'detritivores_*_{init_time}.nc'))[0])
-        detritus = xr.open_dataarray(
-            glob(os.path.join(out_folder, f'detritus_*_{init_time}.nc'))[0])
-
-    #Create dataset for predator, detritivores and detritus initialisation data
-    ds_init = xr.Dataset(data_vars = {'predators': predators, 
-                                      'detritivores': detritivores, 
-                                      'detritus': detritus})
+        ds_init = uf.loading_dbpm_biomass_inputs(out_folder, init_time)
     # #Scatter initialisation dataset across workers
     # ds_init_scattered = client.scatter(ds_init)
     # #Complete scattering before use ("dask future")
     # ds_init_fut = ds_init_scattered.result()
 
     ## Loading dynamic data ----
-    init_yr = pd.Timestamp(init_time).year
+    gridded_esm = os.path.join(base_folder, 'gridded', model_res)
+    ds_dynamic = uf.loading_dbpm_dynamic_inputs(gridded_esm, gridded_folder,
+                                                init_time = init_time)
+    if init_time is not None:
+        init_yr = pd.Timestamp(init_time).year
+        #Timestep from when to restart DBPM 
+        subset_time = (pd.Timestamp(init_time)+pd.DateOffset(months = 1)).strftime('%Y-%m')
+        #Timestep from when to add init effort data
+        effort_time = (pd.Timestamp(init_time)+pd.DateOffset(months = 2)).strftime('%Y-%m')
+        
     #Spinup data is loaded if init_time is None or if the init_time year is less than 1960
     if init_time is None or init_yr < 1959:
-        #Intercept plankton size spectrum
-        ui0 = xr.open_zarr(glob(os.path.join(gridded_folder,
-                                             'ui0_spinup*'))[0])['ui0']
-        #Slope plankton size spectrum
-        slope = xr.open_zarr(glob(
-            os.path.join(base_folder, 'gridded', 
-                         model_res, '*spinup_slope_*'))[0])['slope']
-        #Temperature effects
-        pel_tempeffect = xr.open_zarr(glob(
-            os.path.join(gridded_folder, 
-                         'pel-temp-eff_spinup*'))[0])['pel_temp_eff']
-        ben_tempeffect = xr.open_zarr(glob(
-            os.path.join(gridded_folder, 
-                         'ben-temp-eff_spinup*'))[0])['ben_temp_eff']
-        #Sinking rate
-        sinking_rate = xr.open_zarr(glob(
-            os.path.join(base_folder, 'gridded', model_res,
-                         '*_spinup_er_*'))[0])['export_ratio']
         # Loading effort
-        effort = xr.open_zarr(glob(os.path.join(gridded_folder, 
-                                                'effort_spinup*'))[0])['effort']
+        effort = xr.open_zarr(glob(os.path.join(gridded_folder, 'effort_spinup*'))[0])['effort']
     #Spinup data plus obsclim are loaded if init_time is 1960
     elif init_yr >= 1959 and init_yr < 1961:
-        exp = ['spinup', 'obsclim']
-        #Intercept plankton size spectrum
-        ui0 = xr.open_mfdataset(glob(os.path.join(gridded_folder, 'ui0_*')), 
-                                engine = 'zarr')['ui0']
-        #Slope plankton size spectrum
-        slope = xr.open_mfdataset([f for ex in exp for f in glob(
-            os.path.join(base_folder, 'gridded', model_res, f'*{ex}_slope_*'))], 
-                                  engine = 'zarr')['slope']
-        #Temperature effects
-        pel_tempeffect = xr.open_mfdataset(glob(
-            os.path.join(gridded_folder, 'pel-temp-eff_*')),
-                                           engine = 'zarr')['pel_temp_eff']
-        ben_tempeffect = xr.open_mfdataset(glob(
-            os.path.join(gridded_folder, 'ben-temp-eff_*')),
-                                           engine = 'zarr')['ben_temp_eff']
-        #Sinking rate
-        sinking_rate = xr.open_mfdataset([f for ex in exp for f in glob(
-            os.path.join(base_folder, 'gridded', model_res, f'*{ex}_er_*'))], 
-                                         engine = 'zarr')['export_ratio']
         # Loading effort
         effort = xr.open_mfdataset(glob(os.path.join(gridded_folder, 'effort_*')),
                                    engine = 'zarr')['effort']
     #Obsclim data loaded from 1961 onwards:
     else:
-        #Intercept plankton size spectrum
-        ui0 = xr.open_zarr(glob(
-            os.path.join(gridded_folder, 'ui0_[0-9]*'))[0])['ui0']
-        #Slope plankton size spectrum
-        slope = xr.open_zarr(glob(
-            os.path.join(base_folder, 'gridded', 
-                         model_res, '*obsclim_slope_*'))[0])['slope']
-        #Temperature effects
-        pel_tempeffect = xr.open_zarr(glob(
-            os.path.join(gridded_folder, 'pel-temp-eff_[0-9]*'))[0])['pel_temp_eff']
-        ben_tempeffect = xr.open_zarr(glob(
-            os.path.join(gridded_folder, 'ben-temp-eff_[0-9]*'))[0])['ben_temp_eff']
-        #Sinking rate
-        sinking_rate = xr.open_zarr(glob(
-            os.path.join(base_folder, 'gridded', model_res,
-                         '*_obsclim_er_*'))[0])['export_ratio']
         # Loading effort
-        effort = xr.open_zarr(glob(
-            os.path.join(gridded_folder, 'effort_[0-9]*'))[0])['effort']
-
-    #If running from a specific point in time, then data is subsetted from the month after
-    #init_time
+        effort = xr.open_zarr(glob(os.path.join(gridded_folder, 'effort_[0-9]*'))[0])['effort']
+    
     if init_time is not None:
-        #Timestep from when to restart DBPM 
-        subset_time = (pd.Timestamp(init_time)+
-                       pd.DateOffset(months = 1)).strftime('%Y-%m')
-        #Timestep from when to add init effort data
-        effort_time = (pd.Timestamp(init_time)+
-                       pd.DateOffset(months = 2)).strftime('%Y-%m')
-        
-        #begin (i.e., one month after predators data)
-        ui0 = ui0.sel(time = slice(subset_time, None))
-        slope = slope.sel(time = slice(subset_time, None))
-        pel_tempeffect = pel_tempeffect.sel(time = slice(subset_time, None))
-        ben_tempeffect = ben_tempeffect.sel(time = slice(subset_time, None))
-        sinking_rate = sinking_rate.sel(time = slice(subset_time, None))
-        #Effort   
         #Load effort for time step DBPM starts
         e_start = xr.open_dataarray(glob(os.path.join(out_folder, 
                                                       f'effort_*_{subset_time}.nc'))[0])
+        #Subset effort data from the timestep after DBPM restart 
         effort = effort.sel(time = slice(effort_time, None))
         #Combine both data arrays
         effort = xr.concat([e_start, effort], dim = 'time')
-
-    #Create a single dataset for dynamic inputs
-    ds_dynamic = xr.Dataset(data_vars = {'ui0': ui0, 'slope': slope,
-                                         'pel_tempeffect': pel_tempeffect,
-                                         'ben_tempeffect': ben_tempeffect, 
-                                         'sinking_rate': sinking_rate,
-                                         'effort': effort})
+        effort = effort.chunk(ds_dynamic.chunks)
+    
+    #Creating a single dataset for all dynamic inputs
+    ds_dynamic['effort'] = effort
+    ds_dynamic = ds_dynamic.chunk({'time': 1})
     # #Scatter initialisation dataset across workers
     # ds_dynamic_scattered = client.scatter(ds_dynamic)
     # #Complete scattering before use ("dask future")
     # ds_dynamic_fut = ds_dynamic_scattered.result()
 
-    
+    #Gridded parameters
+    gridded_params = json.load(open(
+        glob(os.path.join(gridded_folder, 'dbpm_gridded_*_python.json'))[0]))
+
     ## Running spatial DBPM ----
-    uf.gridded_sizemodel(gridded_folder, ds_fixed, ds_init, ds_dynamic, 
-                         region = region, model_res = model_res, out_folder = out_folder)
+    for t in range(0, len(ds_dynamic.time)):
+        ds_dyn = ds_dynamic.isel(time = t)
+        # Redistribute total effort across grid cells 
+        if ds_dynamic.time[t+1] <= ds_dynamic.time.max():
+            eff_short = uf.effort_calculation(ds_init['predators'], ds_init['detritivores'], 
+                                              ds_dynamic['effort'].isel(time = t+1), 
+                                              ds_fixed['depth'], 
+                                              ds_fixed['log10_size_bins'], 
+                                              gridded_params)
+            # Saving predation mortality
+            #Getting year and month 
+            dt_eff = pd.to_datetime(eff_short.time.values[0]).strftime('%Y-%m')
+            # Creating file name
+            fn = f'effort_{model_res}_{region}_{dt_eff}.nc'
+            eff_short.to_netcdf(os.path.join(out_folder, fn))
+            ds_dynamic['effort'] = xr.where(ds_dynamic.time == ds_dynamic.time[t+1], 
+                                            eff_short.values, ds_dynamic['effort'])
+            #Remove variables not needed
+            del dt_eff, fn
+        
+        ds_init = uf.gridded_sizemodel(gridded_params, ds_fixed, ds_init, 
+                                       ds_dyn, region = region, model_res = model_res, 
+                                       out_folder = out_folder)
 
     #Closing client
     # client.close()
+
