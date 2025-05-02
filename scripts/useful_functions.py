4d8f909c029920840b2400f216d7719247de5bad
lidefi87 <lidefi87@hotmail.com>
2025-05-02 19:36:23 +1000

    processing data for stable spin period

--- scripts/useful_functions.py
+++ scripts/useful_functions.py
@@ -661,7 +661,20 @@ def loading_dbpm_dynamic_inputs(gridded_folder, init_time = None,
     else:
         cap_search = ''
         
-    if init_time is None or init_yr < 1959:
+    if init_time is None or init_yr < 1841:
+        ui0 = xr.open_mfdataset(glob(os.path.join(
+            gridded_calc, f'ui0{cap_search}_stable-spin*')), engine = 'zarr')['ui0']
+        slope = xr.open_mfdataset(glob(os.path.join(
+            gridded_esm, f'*stable-spin_slope{cap_search}_*')), engine = 'zarr')['slope']
+        pel_tempeffect = xr.open_mfdataset(glob(
+            os.path.join(gridded_calc, 'pel-temp-eff_stable-spin*')), 
+                                           engine = 'zarr')['pel_temp_eff']
+        ben_tempeffect = xr.open_mfdataset(glob(os.path.join(
+            gridded_calc, 'ben-temp-eff_stable-spin*')), engine = 'zarr')['ben_temp_eff']
+        sinking_rate = xr.open_mfdataset(
+            glob(os.path.join(gridded_esm, f'*_stable-spin_er{cap_search}_*')),
+            engine = 'zarr')['export_ratio']
+    elif init_time >= 1841 or init_yr < 1959:
         ui0_search = glob(os.path.join(gridded_folder, f'ui0{cap_search}_spinup*')) 
         slope_search = glob(os.path.join(gridded_folder, 
                                          f'*spinup_slope{cap_search}_*'))           
