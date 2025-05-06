27fa5fd06ad47a68d6694408d12276aa958d2b69
lidefi87 <lidefi87@hotmail.com>
2025-05-07 09:26:25 +1000

    removing line not in use

--- scripts/06_run_dbpm_gridded.py
+++ scripts/06_run_dbpm_gridded.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     freeze_support()
 
     #Start a cluster
-    client = Client(threads_per_worker = 1)
+    client = Client(threads_per_worker = 1, memory_limit = 0)
     
     ## Name of region and model resolution ----
     reg = 'fao-47'
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     ## If starting DBPM run from a specific time step ----
     # Character: Year and month from when DBPM initialisation values should be loaded
     # If starting model for the first time, it should be set to None
-    init_time = '1812-07'
+    init_time = '1892-03'
 
     ## Defining input and output folders ----
     base_folder = '/g/data/vf71/fishmip_inputs/ISIMIP3a/fao_inputs'
@@ -56,42 +56,42 @@ if __name__ == '__main__':
         ds_init = uf.loading_dbpm_biomass_inputs(out_folder, init_time)
     
     ## Loading dynamic data ----
-    ds_dynamic = uf.loading_dbpm_dynamic_inputs(gridded_folder, init_time, capped = False)
+    ds_dynamic = uf.loading_dbpm_dynamic_inputs(gridded_folder, init_time, 
+                                                capped = False)
   
     if init_time is not None:
         init_yr = pd.Timestamp(init_time).year
         #Timestep from when to restart DBPM 
-        subset_time = (pd.Timestamp(init_time)+pd.DateOffset(months = 1)).strftime('%Y-%m')
+        subset_time = (pd.Timestamp(init_time)+
+                       pd.DateOffset(months = 1)).strftime('%Y-%m')
         #Timestep from when to add init effort data
-        effort_time = (pd.Timestamp(init_time)+pd.DateOffset(months = 2)).strftime('%Y-%m')
+        effort_time = (pd.Timestamp(init_time)+
+                       pd.DateOffset(months = 2)).strftime('%Y-%m')
         #Load effort for time step DBPM starts
         e_start = xr.open_dataarray(glob(os.path.join(
             out_folder, f'effort_*_{subset_time}.nc'))[0])
         #Subset effort data from the timestep after DBPM restart 
-        effort = ds_dynamic['effort'].sel(time = slice(effort_time, 
-                                                       None))
+        effort = ds_dynamic['effort'].sel(time = slice(effort_time, None))
         #Combine both data arrays
         effort = xr.concat([e_start, effort], dim = 'time')
-        effort = effort.chunk({'lat': len(effort.lat), 
-                               'lon': len(effort.lon),
+        effort = effort.chunk({'lat': len(effort.lat), 'lon': len(effort.lon),
                                'time': -1})
     
         #Creating a single dataset for all dynamic inputs
         ds_dynamic['effort'] = effort
-        ds_dynamic = ds_dynamic.chunk({'time': 1})
     
     #Gridded parameters
     gridded_params = json.load(open(
-        glob(os.path.join(
-            base_folder, reg, 'fishing_params', res, 'best_fish_vals',
-            'dbpm_gridded_*_python.json'))[0]))
+        glob(os.path.join(base_folder, reg, 'fishing_params', res, 
+                          'best_fish_vals', 'dbpm_gridded_*_python.json'))[0]))
 
     ## Running spatial DBPM ----
     for t in range(0, len(ds_dynamic.time)):
         ds_dyn = ds_dynamic.isel(time = t)
         # Redistribute total effort across grid cells 
         try:
-            eff_short = uf.effort_calculation(ds_init['predators'], ds_init['detritivores'], 
+            eff_short = uf.effort_calculation(ds_init['predators'], 
+                                              ds_init['detritivores'], 
                                               ds_dynamic['effort'].isel(time = t+1), 
                                               ds_fixed['depth'], 
                                               ds_fixed['log10_size_bins'], 
@@ -108,7 +108,8 @@ if __name__ == '__main__':
             del dt_eff, fn
         except:
             dt = pd.to_datetime(ds_dyn.time.values).strftime('%Y-%m')
-            eff_short = xr.open_dataarray(glob(os.path.join(out_folder, f'effort*{dt}*'))[0])
+            eff_short = xr.open_dataarray(glob(os.path.join(out_folder, 
+                                                            f'effort*{dt}*'))[0])
             ds_dynamic['effort'] = xr.where(ds_dynamic.time == ds_dynamic.time[t], 
                                             eff_short.values, ds_dynamic['effort'])
         
