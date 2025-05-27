2709372e4a609ed5dd0ad0b55956404232a0b89d
lidefi87 <lidefi87@hotmail.com>
2025-05-27 16:32:53 +1000

    adding parameter to choose whether or not to include fishing

--- scripts/useful_functions.py
+++ scripts/useful_functions.py
@@ -635,13 +635,15 @@ def loading_dbpm_biomass_inputs(data_folder, init_time = None):
 
 
 # Loading initialising values for gridded DBPM biomass ------
-def loading_dbpm_dynamic_inputs(gridded_folder, init_time = None,
+def loading_dbpm_dynamic_inputs(gridded_folder, init_time = None, fishing = True,
                                 capped = False):
     '''
     Inputs:
     - gridded_esm (character) Full path to folder where processed DBPM inputs are stored
     - init_time (character) Default is None. Year and month from when to restart gridded 
     DBPM runs. If set to None, DBPM is run from the beginning
+    - fishing (boolean) Default is True. It indicates whether or not fishing effort 
+    should be included in the dynamic inputs dataset
     - capped (boolean) Default is None. It indicates if "capped" inputs should be
     used in the DBPM run.
 
@@ -686,9 +688,14 @@ def loading_dbpm_dynamic_inputs(gridded_folder, init_time = None,
         sinking_rate_search, engine = 'zarr', 
         parallel = True)['export_ratio'].chunk({'time': -1, 'lon': -1, 'lat': -1})
 
-    [effort_search] = glob(os.path.join(gridded_folder, 'effort_spinup_obsclim*'))
-    effort = xr.open_zarr(
-        effort_search, chunks = {'time': -1, 'lon': -1, 'lat': -1})['effort']
+    if fishing:
+        [effort_search] = glob(os.path.join(gridded_folder, 'effort_spinup_obsclim*'))
+        effort = xr.open_zarr(
+            effort_search, chunks = {'time': -1, 'lon': -1, 'lat': -1})['effort']
+    else:
+        effort = xr.zeros_like(ui0)
+        effort = effort.where(np.isfinite(ui0))
+        effort.name = 'effort'
 
     #Subset data
     if init_time is not None:
