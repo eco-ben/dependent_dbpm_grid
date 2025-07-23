206fea0239ec906ecd6c81d8f995545b00e0c038
lidefi87 <lidefi87@hotmail.com>
2025-07-23 14:19:07 +1000

    testing seasonal_decomposition function

--- scripts/02b_smoothing_DBPM_regional_inputs.py
+++ scripts/02b_smoothing_DBPM_regional_inputs.py
@@ -6,20 +6,6 @@ import useful_functions as uf
 from glob import glob
 import shutil
 
-# Gaussian smoothing
-# import xarray as xr
-# from scipy.ndimage import gaussian_filter1d
-# def apply_gaus_fil(da, sigma):
-#     return xr.apply_ufunc(
-#         gaussian_filter1d,
-#         da,
-#         input_core_dims = [['time']],
-#         output_core_dims = [['time']],
-#         kwargs = {'sigma': sigma},
-#         dask = 'parallelized', # For larger datasets, enables Dask
-#         output_dtypes = [da.dtype]
-#     )
-
 # Defining base folder
 base_dir = '/g/data/vf71/fishmip_inputs/ISIMIP3a/fao_inputs/'
 # Getting list of FAO regions
@@ -33,26 +19,26 @@ resolutions = ['1deg']
 for fao, res in zip(fao_code, resolutions):
     file_list = glob(os.path.join(base_dir, f'fao-{fao}', 'gridded', res,
                                   'gfdl-*'))
-    folder_out = os.path.join(base_dir, f'fao-{fao}', 'gridded-smoothed', res)
+    # folder_out = os.path.join(base_dir, f'fao-{fao}', 'gridded-smoothed', res)
+    folder_out = os.path.join(base_dir, f'fao-{fao}', 'gridded-deseasoned', res)
     os.makedirs(folder_out, exist_ok = True)
 
     #Extracting data for FAO area
     for f in file_list:
-        if 'monthly' in f:
+        # if 'monthly' in f:
+        if 'monthly' in f and 'stable-spin' not in f:
             #Create file path to save outputs
             f_out = os.path.basename(f).replace('_monthly_', 
                                                 '_monthly-smoothed_')
             path_out = os.path.join(folder_out, f_out)
-            uf.smoothing_loess(f, path_out, frac = 12, it = 0)
-            # # Loading data
-            # da = xr.open_zarr(f)
-            # #Getting name of variable contained in dataset
-            # [var] = list(da.keys())
-            # da = da[var]
-            # da_gf = apply_gaus_fil(da, 1)
-            # da_gf.to_zarr(path_out, consolidated = True, mode = 'w')
+            # LOESS smoothing
+            # uf.smoothing_loess(f, path_out, frac = 12, it = 0)
+            # Seasonal decomposition 
+            uf.seasonal_decomposition(f, path_out, period = 12, 
+                                      component = 'seasonal')
         else:            
-            f_out = f.replace('gridded/', 'gridded-smoothed/')
+            # f_out = f.replace('gridded/', 'gridded-smoothed/')
+            f_out = f.replace('gridded/', 'gridded-deseasoned/')
             try:
                 shutil.copytree(f, f_out)
             except:
