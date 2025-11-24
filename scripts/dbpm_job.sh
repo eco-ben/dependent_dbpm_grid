cee8b88ed3ec9dfb6ea33442683cfb0d3d3760b6
lidefi87 <lidefi87@hotmail.com>
2025-11-24 16:06:35 +1100

    upgrading conda environment

--- scripts/dbpm_job.sh
+++ scripts/dbpm_job.sh
@@ -2,7 +2,7 @@
 
 #PBS -P vf71
 #PBS -q normalbw
-#PBS -l ncpus=56
+#PBS -l ncpus=28
 #PBS -l mem=126GB
 #PBS -l walltime=24:00:00
 #PBS -l storage=gdata/vf71+gdata/xp65
@@ -11,5 +11,5 @@
 #PBS -l wd
 
 module use /g/data/xp65/public/modules
-module load conda/analysis3-25.04
-python3 06_run_dbpm_gridded.py
+module load conda/analysis3-25.11
+python3 07_run_dbpm_gridded.py
