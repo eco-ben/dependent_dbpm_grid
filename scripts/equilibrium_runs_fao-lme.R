dfcff7271829f4471eb968d419b3cefbd6baed51
lidefi87 <lidefi87@hotmail.com>
2026-04-09 15:38:40 +1000

    extra step identifying density and growth for final timestep

--- scripts/equilibrium_runs_fao-lme.R
+++ scripts/equilibrium_runs_fao-lme.R
@@ -16,7 +16,7 @@ library(tibble)
 
 # Loading DBPM climate and fishing inputs ---------------------------------
 base_folder <- "/g/data/vf71/fishmip_inputs/ISIMIP3a/fao_lme_inputs"
-out_folder <- "/g/data/vf71/fishmip_outputs/ISIMIP3a/fao_lme_outputs/"
+out_folder <- "/g/data/vf71/fishmip_outputs/ISIMIP3a/fao_lme_outputs"
 
 fao_lme <- list.dirs(base_folder, recursive = F, full.names = F) |> 
   str_subset(pattern = "fao_lme-")
@@ -258,12 +258,12 @@ for(f in fao_lme){
 }
 
 
-# Creating diagnostic plots per region ------------------------------------
+# Creating diagnostic plots per region per decade -------------------------
 for(f in fao_lme){
   results_folder <- file.path(out_folder, f, "equilibrium_run")
   
   print(paste0("Running DBPM for region: ", f))
-
+  
   ## Size spectrum plots --------------------------------------------------
   # Loading data
   density_df <- list.files(results_folder, pattern = "size_spectrum_data_", 
@@ -290,20 +290,20 @@ for(f in fao_lme){
   
   # Saving plot
   ggsave(file.path(results_folder,
-                     paste0("size_spectrum_", f, "_searchvol-check", ".png")),
+                   paste0("size_spectrum_", f, "_searchvol-check", ".png")),
          density_fig, bg = "white")
   
   ## Growth rate plots ----------------------------------------------------
   # Loading data
   growth_df <- list.files(results_folder, pattern = "growth_rates_data", 
-                           full.names = T) |> 
+                          full.names = T) |> 
     map(\(x) read_parquet(x)) |> 
     bind_rows() |> 
     # Removing rows with no data
     drop_na(growth) |> 
     # Removing rows where growth rate is equal or less than 0
     filter(growth > 0) 
-
+  
   # Plotting data for last decade
   growth_fig <- growth_df |> 
     filter(decade == max(decade)) |> 
@@ -335,13 +335,16 @@ for(f in fao_lme){
     # Removing rows where growth rate is equal or less than 0
     filter(values >= 0) 
   
-  bad_search_vol <- bio_df |> 
-    count(search_vol, group) |> 
-    filter(n < 200)
+  good_search_vol <- bio_df |> 
+    filter(group != "plankton") |> 
+    count(search_vol) |> 
+    # A total of 600, which includes 200 timesteps and three groups (predators,
+    # detritivores and detritus)
+    filter(n == 600) 
   
   # Plotting data for entire simulation
   bio_fig <- bio_df |> 
-    filter(!search_vol %in% bad_search_vol$search_vol) |> 
+    filter(search_vol %in% good_search_vol$search_vol) |> 
     ggplot(aes(year, values, colour = factor(search_vol), 
                group = search_vol))+
     geom_line(alpha = 0.5)+
@@ -358,3 +361,109 @@ for(f in fao_lme){
                           "_searchvol-check", ".png")),
          bio_fig, bg = "white")
 }
+
+
+
+# Creating diagnostic plots per region for final timestep -----------------
+density_growth_global <- tibble()
+for(f in fao_lme){
+  results_folder <- file.path(out_folder, f, "equilibrium_run")
+  
+  print(paste0("Running DBPM for region: ", f))
+  
+
+  ## Regional inputs ------------------------------------------------------
+  f_inputs <- read_parquet(list.files(
+    file.path(base_folder, f, paste0("monthly_weighted", smoothed)), 
+    paste0("dbpm_clim-fish-inputs", fn_search, "_", f), full.names = T), 
+    col_select = scenario:depth) |> 
+    filter(time == min(time))
+  
+  ## Biomass plots --------------------------------------------------------
+  # Loading data
+  bio_df <- list.files(results_folder,
+                       "plankton-pred-detritus-bio_detritus_.*parquet", 
+                       full.names = T) |> 
+    map(\(x) read_parquet(x)) |> 
+    bind_rows() |> 
+    # Removing rows with no data
+    drop_na(values) |> 
+    # Removing rows where biomass is 0 or more
+    filter(values >= 0) 
+  
+  good_search_vol <- bio_df |> 
+    filter(group != "plankton") |> 
+    count(search_vol) |> 
+    # A total of 600, which includes 200 timesteps and three groups (predators,
+    # detritivores and detritus)
+    filter(n == 600) 
+  
+  # Get files for successful runs only
+  init_files <- list.files(results_folder, 
+                           pattern = paste(
+                             paste0("^init_dbpm.*_", good_search_vol$search_vol, 
+                                    ".json"), collapse = "|"), full.names = T)
+  
+  
+  density_growth_reg <- tibble()
+  
+  for(fn in init_files){
+    init_results <- read_json(fn, simplifyVector = T)
+    
+    min_pred_size <- init_results$params$log10_size_bins[
+      init_results$params$ind_min_pred_size]
+    min_det_size <- init_results$params$log10_size_bins[
+      init_results$params$ind_min_detritivore_size]
+    
+    # Processing density data for size spectrum
+    density_growth_df <- data.frame(
+      size_class = init_results$params$log10_size_bins, 
+      pred_den = init_results$predators[, ncol(init_results$predators)], 
+      detrit_den = init_results$detritivores[, ncol(init_results$detritivores)],
+      # Growth from time step before end of simulation
+      pred_growth = init_results$growth_int_pred[, ncol(init_results$growth_int_pred)-1], 
+      detrit_growth = init_results$growth_det[, ncol(init_results$growth_det)-1], 
+      search_vol = init_results$params$hr_volume_search) |> 
+      mutate(size_class_g = 10**size_class, .before = pred_growth) |>
+      mutate(pred_den = ifelse(size_class < min_pred_size, NA, pred_den),
+             detrit_den = ifelse(size_class < min_det_size, NA, detrit_den),
+             pred_growth = ifelse(size_class < min_pred_size, NA, pred_growth),
+             detrit_growth = ifelse(size_class < min_det_size, NA, detrit_growth)) |> 
+      drop_na(pred_den:detrit_growth) |> 
+      rowwise() |> 
+      mutate(total = sum(pred_den, detrit_den, na.rm = T), 
+             .after = detrit_den) |>
+      ungroup() |> 
+      mutate(across(c(pred_den, detrit_den, total), log10)) 
+  
+    density_growth_reg <- density_growth_reg |> 
+      bind_rows(density_growth_df)
+  }
+  
+  # Binding all search volumes together within a single region
+  density_growth_reg <- f_inputs |> 
+    bind_cols(density_growth_reg)
+  
+  density_growth_reg |> 
+    write_parquet(
+      file.path(results_folder, 
+                paste0("successful_runs_inputs-outputs_", f, ".parquet")))
+}
+
+
+# Binding all search volumes together within a single region
+density_growth_global <- density_growth_global |> 
+  bind_rows(density_growth_df)
+
+density_growth_global |> 
+  write_parquet(file.path(out_folder, 
+                          "successful_runs_inputs-outputs_global.parquet"))
+
+
+# density_growth_global |> 
+#   summarise(min_sv = min(search_vol), max_sv = max(search_vol), .by = region)
+  
+
+
+
+
