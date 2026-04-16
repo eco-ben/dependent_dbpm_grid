65fc622b0c59b3ffb5b8b74585a7e13fe2ca629a
lidefi87 <lidefi87@hotmail.com>
2026-04-16 11:40:58 +1000

    finalising no-fishing run scripts

--- scripts/06_no-fishing_runs_fao-lme.R
+++ scripts/06_no-fishing_runs_fao-lme.R
@@ -10,8 +10,6 @@ library(dplyr)
 library(arrow)
 library(jsonlite)
 library(purrr)
-# library(GGally)
-# library(tibble)
 
 
 # Loading DBPM climate and fishing inputs ---------------------------------
@@ -38,11 +36,19 @@ if(!is.null(smoothed)){
 out_folder <- "/g/data/vf71/fishmip_outputs/ISIMIP3a/fao_lme_outputs"
 
 # Start calibration runs
-for(f in fao){
+for(f in fao_lme){
+  # Folder where equilibrium run results are stored
   if(f == "fao_lme-64"){
-    results_folder <- file.path(out_folder, f, "dynamic_equilibrium_run")
+    equilibrium_folder <- file.path(out_folder, f, "dynamic_equilibrium_run")
   }else{
-    results_folder <- file.path(out_folder, f, "equilibrium_run")
+    equilibrium_folder <- file.path(out_folder, f, "equilibrium_run")
+  }
+  
+  # Folder to store simulation results
+  results_folder <- file.path(out_folder, f, "no-fishing_run")
+  # If folder does not exist, then create a new one
+  if(!dir.exists(results_folder)){
+    dir.create(results_folder)
   }
   
   # Print message to console to keep track of region being processed
@@ -50,7 +56,7 @@ for(f in fao){
   
   # Load results from equilibrium run
   init_results <- read_json(list.files(
-    results_folder, pattern = paste0("^init_dbpm.*_", search_volume, ".json"),
+    equilibrium_folder, pattern = paste0("init_dbpm.*", search_volume, ".json"),
     full.names = T), simplifyVector = T)
   
   # Load DBPM inputs - Excluding inputs from stable spinup
@@ -60,11 +66,15 @@ for(f in fao){
                      "_1641-2010.parquet"))) |> 
     filter(scenario != "stable-spin")
   
+  # Setting fishing parameters to zero (no-fishing)
   fish_param <- data.frame("region" = str_replace(str_to_upper(f), "-", " "),
                            "fmort_u" = 0, "fmort_v" = 0, "fminx_u" = 0, 
                            "fminx_v" = 0, "search_vol" = search_volume)
   
   ts_stablespin <- init_results$params$numb_time_steps
+  
+  end_fn <- paste0("_", f, "_searchvol-", search_volume, "_", 
+                   min(dbpm_inputs$year), "-", max(dbpm_inputs$year))
   
   # Selecting values for last time step in stable spin period
   # Transforming predators and detritivores from gm-2yr-1 to gm-3yr-1
@@ -80,19 +90,38 @@ for(f in fao){
                               detritivore_initial = detritivore_initial,
                               detritus_initial = detritus_initial, 
                               set_plankton = T, use_init = T)
+  # Saving no-fishing results
+  no_fishing_run |> 
+    #Ensuring up to 10 decimal places are saved in file
+    write_json(file.path(
+      results_folder, paste0("dbpm_no-fishing_nonspatial", end_fn)), 
+      digits = 10)
   
-  
+  # Getting detritivore and predator density estimates
   density_df <- dbpm_output_mat_to_df(no_fishing_run, dbpm_inputs$time,
                                       "density")
+  # Saving no-fishing density results
+  density_df |> 
+    #Ensuring up to 10 decimal places are saved in file
+    write_parquet(file.path(results_folder, 
+                            paste0("size-spectrum", end_fn, ".parquet")))
   
-  # Create size spectrum plots
+  ## Creating size spectrum plots -----------------------------------------
+  # Decade
   size_sp_plot <- plotsizespectrum(density_df, no_fishing_run$params, f, 
                                    fishing_params = fish_param, mean_decade = T,
                                    nrow = 3)
-  
+  # Last year of simulation
   size_sp_plot2 <- plotsizespectrum(density_df, no_fishing_run$params, f, 
-                                   fishing_params = fish_param, mean_decade = F)
+                                    fishing_params = fish_param, mean_decade = F)
   
+  # Saving size spectrum plots
+  ggsave(
+    file.path(results_folder, paste0("size-spectrum-decade", end_fn, ".png")),
+    size_sp_plot, bg = "white")
+  
+  ggsave(file.path(results_folder, paste0("size-spectrum-final-year", end_fn,
+                                          ".png")), size_sp_plot, bg = "white")
   
   ## Creating growth rate plots -------------------------------------------
   dates_model <- c(min(dbpm_inputs$time)%m-% months(1), dbpm_inputs$time)
@@ -101,16 +130,31 @@ for(f in fao){
     # initialisation only
     filter(time >= min(as_date(dbpm_inputs$time)))
   
+  # Saving no-fishing growth results
+  growth_df |> 
+    #Ensuring up to 10 decimal places are saved in file
+    write_parquet(file.path(results_folder, 
+                            paste0("growth-rates", end_fn, ".parquet")))
+  
   # Create growth rate plot
   growth_plot <- growth_df |> 
     filter(decade == max(decade)) |> 
     plot_growth_rate(no_fishing_run$params, f, fishing_params = fish_param)
   
-  growth_df |> 
+  growth_plot2 <- growth_df |> 
     filter(time != max(time)) |> 
     filter(time == max(time)) |> 
     plot_growth_rate(no_fishing_run$params, f, fishing_params = fish_param)
   
+  # Saving growth plots
+  ggsave(
+    file.path(results_folder, paste0("growth-rates-decade", end_fn, ".png")),
+    growth_plot, bg = "white")
+  
+  ggsave(file.path(results_folder, paste0("growth-rates-final-year", end_fn,
+                                          ".png")), growth_plot2, bg = "white")
+  
+  ## Creating biomass plots ---------------------------------------------
   no_fishing_run_bio <- run_model(fish_param, dbpm_inputs, withinput = T, 
                                   xmin_consumer_u = -3, xmin_consumer_v = -3, 
                                   include_plankton = T, 
@@ -118,6 +162,12 @@ for(f in fao){
                                   detritivore_initial = detritivore_initial,
                                   detritus_initial = detritus_initial, 
                                   set_plankton = T, use_init = T)
+  
+  # Saving no-fishing results
+  no_fishing_run_bio |> 
+    #Ensuring up to 10 decimal places are saved in file
+    write_parquet(file.path(
+      results_folder, paste0("dbpm-biomass_no-fishing_nonspatial", end_fn)))
   
   biomass_data <- no_fishing_run_bio |>
     select(year, ends_with("biomass"), total_detritus) |>
@@ -140,6 +190,11 @@ for(f in fao){
                   ": Predator and detritivore biomass, and detritus density"))+
     theme_bw()+
     theme(axis.title.x = element_blank())
+  
+  ggsave(
+    file.path(results_folder, paste0("plankton-pred-detritus-bio_detritus", 
+                                     end_fn, ".png")), bio_plot, bg = "white")
+  
 }
 
   
